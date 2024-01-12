867f76d595e3fefa8fe3d64ebfd6d8da2b0648d7
Bejar L <143468066+BejarL@users.noreply.github.com>
2024-01-12 15:09:17 -0800

    Finished Meme Genrator

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header.jsx"
+import Meme from "./components/Meme.jsx"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'
+
+function Main() {
+  return (
+    <App />
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />); 
